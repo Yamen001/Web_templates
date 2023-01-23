5583069549825e5e41ad8cb5ad3e1fa6d1d5ff94
yamen <hzmkiller@gmail.com>
2023-01-23 23:47:21 +0300

    new project

---
+++ business/reacttemplate/src/App.js
@@ -0,0 +1,17 @@
+import './styles/App.css';
+import Navbar from './Components/nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      {/* <Landing /> */}
+      {/* <Text /> */}
+      {/* <Products /> */}
+      {/* <Team /> */}
+      {/* <Gallart /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ business/reacttemplate/src/Components/nav.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "../styles/nav.css"
+let menu = document.querySelector("nav .Menu")
+console.log(menu)
+export default function Navbar(){
+    return(
+        <nav>
+            <div className="container">
+                <div className="search">
+                    <input type="text" placeholder="Search" />
+                    <button className="btn-search"><i className="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+                <div className="hamburger" onClick={openMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className="Menu close">
+                    <div className="list">
+                        <button className="close" onClick={closeMenu} >X</button>
+                        <ul>
+                            <li> <a href="s">Home</a> </li>
+                            <li> <a href="s">About Us</a> </li>
+                            <li> <a href="s">Contact Us</a> </li>
+                            <li> <a href="s">Blog</a> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+function openMenu(){
+    menu.classList.remove("close")
+    menu.classList.add("open")
+}
+function closeMenu(){
+    menu.classList.remove("oepn")
+    menu.classList.add("close")
+}
+// show the menu...just edit the classes
