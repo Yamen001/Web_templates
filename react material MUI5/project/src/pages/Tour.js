0f9004b4859f8019dc152365bf8918ab462ee3cb
yamen <hzmkiller@gmail.com>
2023-03-04 01:33:02 +0300

    new changes

--- react material MUI5/project/src/pages/Tour.js
+++ react material MUI5/project/src/pages/Tour.js
@@ -1,8 +1,22 @@
+import { Box, Container, Typography } from '@mui/material'
 import React from 'react'
 
 function Tour() {
     return (
-        <div>hello</div>
+        <Container sx={{ width: 900 }}>
+            <Typography variant='h3' component='h1' marginTop={3}>
+                Export The world Vegas
+            </Typography>
+            <Box margintTop={4} sx={{ display: "flex" }}>
+                <img
+                    src=  "https://footwearnews.com/wp-content/uploads/2020/10/magic-trade-show-las-vegas.jpg"
+                    alt=""
+                    height={200}
+                />
+
+            </Box>
+        </Container>
     )
+
 }
 export default Tour
